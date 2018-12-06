e17b4b4a7ae25ac2b34a88971b2e5962ac290771
Sukmo Afri Ardi Saputro <qifuataufiqs@gmail.com>
2018-12-06 21:06:09 +0700

    ListView + OptionsMenu

--- app/src/main/java/com/qifuataufiqs/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/qifuataufiqs/myrecyclerview/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         rvCategory = (RecyclerView)findViewById(R.id.rv_category);
         rvCategory.setHasFixedSize(true);
 
@@ -28,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
         CorehaqAdapter listCoreAdapter = new CorehaqAdapter(this);
         listCoreAdapter.setListCoreVisual(list);
         rvCategory.setAdapter((listCoreAdapter));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                break;
+            case R.id.action_grid:
+                break;
+            case R.id.action_cardview:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
